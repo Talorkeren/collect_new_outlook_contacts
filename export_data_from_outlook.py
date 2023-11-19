428a6ea3a001dba2d1b7065e0a4a401a729a44ee
Talor Keren <talor.k@cropx.com>
2023-11-19 14:47:38 +0200

    Updated file export_data_from_outlook.py to get contacts from Calendar.

--- export_data_from_outlook.py
+++ export_data_from_outlook.py
@@ -10,12 +10,13 @@ contacts_file = ContactsFile()
 
 class OutLook:
     def __init__(self):
+        self.meetings = None
         self.inbox = None
         self.outlook = None
         self.list_of_contacts = []
         today = datetime.datetime.now()
         self.end_date = datetime.datetime(year=today.year, month=today.month, day=today.day, tzinfo=timezone('UTC'))
-        self.start_date = datetime.datetime(year=2023, month=9, day=10, tzinfo=timezone('UTC'))
+        self.start_date = datetime.datetime(year=2023, month=3, day=10, tzinfo=timezone('UTC'))
 
 
     def check_output_folder(self):
@@ -27,24 +28,34 @@ class OutLook:
         try:
             self.outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
             # self.inbox = self.outlook.Folders("talor.keren1@gmail.com").Folders("Inbox")
-            self.inbox = self.outlook.GetDefaultFolder(5)
+            self.meetings = self.outlook.GetDefaultFolder(9)
+            # print(self.inbox.Items)
+            # for x in self.inbox.Items:
+            #     # print(x)
+            #     subject = x.Subject
+            #     for y in x.recipients:
+            #         print(y.Adress)
+
+                # print(subject)
+
         except:
             print("try again")
 
     def get_messages(self):
-        messages = self.inbox.Items
-        messages_counter = messages.Count
-        print(messages_counter)
-        for message in messages:
-            # subject = message.Subject
+        meets = self.meetings.Items
+        meets_counter = meets.Count
+        print(meets_counter)
+        for meet in meets:
+            # meet_subject = meet.Subject
+            # print(meet_subject)
             # body = message.body
 
-            sent_date = message.ReceivedTime
-            if self.start_date < sent_date < self.end_date:
-                for recip in message.recipients:
-                    if "/o=ExchangeLabs/ou=Exchange Administrative Group (FYDIBOHF23SPDLT" not in recip.Address:
-                        if recip.Address.lower() not in self.list_of_contacts:
-                            self.list_of_contacts.append(recip.Address.lower())
+            # meet_date = meet.ReceivedTime
+            # if self.start_date < meet_date < self.end_date:
+            for recip in meet.recipients:
+                if "/o=ExchangeLabs/ou=Exchange Administrative Group (FYDIBOHF23SPDLT" not in recip.Address and "cropx.com" not in recip.Address:
+                    if recip.Address.lower() not in self.list_of_contacts:
+                        self.list_of_contacts.append(recip.Address.lower())
 
     def main(self):
         self.check_output_folder()
